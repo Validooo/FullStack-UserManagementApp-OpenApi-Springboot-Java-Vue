aa6b8237950368fcd4c45b33299c48f1dde9916e
Walid El Kassem <walid.elkassem99@gmail.com>
2023-08-25 17:13:12 +0200

    change application port number

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,4 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   publicPath: '/FullStack-UserManagementApp-OpenApi-Springboot-Java-Vue/',
   transpileDependencies: true,
+  devServer: {
+    port: 3000 // Change to the desired port number
+  }
 })
